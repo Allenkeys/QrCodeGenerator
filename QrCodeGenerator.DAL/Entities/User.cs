7c75016fd583f520011a3b8adc3746faffa7baa2
Joshua Eze <ezejoshuac@gmail.com>
2023-03-13 21:59:00 +0100

    Update user model

--- QrCodeGenerator.DAL/Entities/User.cs
+++ QrCodeGenerator.DAL/Entities/User.cs
@@ -4,7 +4,9 @@ namespace QrCodeGenerator.DAL.Entities
 {
     public class User : BaseEntity
     {
-        public string FullName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? MiddleName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public IList<QrCode> QrCodes { get; set; }
