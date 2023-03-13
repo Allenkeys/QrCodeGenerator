f7b378869500dfd77783f3abd1013ce445f6a947
Joshua Eze <ezejoshuac@gmail.com>
2023-03-13 13:57:10 +0100

    Added DAL dependencies of efcore
    - Extended DbContext for database module

---
+++ QrCodeGenerator.DAL/Database/QrCodeDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using QrCodeGenerator.DAL.Entities;
+
+namespace QrCodeGenerator.DAL.Database
+{
+    internal class QrCodeDbContext : DbContext
+    {
+        public QrCodeDbContext(DbContextOptions<QrCodeDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<QrCode> Tasks { get; set; }
+    }
+}
