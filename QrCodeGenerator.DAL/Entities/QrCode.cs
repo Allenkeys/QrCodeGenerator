12636da42e74638a01333898ba8ff03148321889
Joshua Eze <ezejoshuac@gmail.com>
2023-03-13 13:45:18 +0100

    Added qrcode entity

---
+++ QrCodeGenerator.DAL/Entities/QrCode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QrCodeGenerator.DAL.Entities
+{
+    public class QrCode
+    {
+        public byte[] Code { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
