02dea59b41758c5a8a587cf135cce8627af30087
Joshua Eze <ezejoshuac@gmail.com>
2023-03-13 21:59:35 +0100

    Register sql connection

--- QrCodeGenerator.MVC/Program.cs
+++ QrCodeGenerator.MVC/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using QrCodeGenerator.DAL.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<QrCodeDbContext>(option => option
+    .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
