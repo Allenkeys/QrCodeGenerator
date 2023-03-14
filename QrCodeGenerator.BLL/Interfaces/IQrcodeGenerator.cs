ecef303e30def59ee214d4d373eb0681236cc8c1
Joshua Eze <ezejoshuac@gmail.com>
2023-03-14 12:59:57 +0100

    Added services interfaces

---
+++ QrCodeGenerator.BLL/Interfaces/IQrcodeGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+
+namespace QrCodeGenerator.BLL.Interfaces
+{
+    public interface IQrcodeGenerator
+    {
+        public Byte[] GernerateQR(string text);
+    }
+}
