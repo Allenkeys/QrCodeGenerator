a371ebd5644e1a462c94b932c4f0673cc18ec96b
Joshua Eze <ezejoshuac@gmail.com>
2023-03-14 13:01:26 +0100

    Configure dbcontext with fluent api

---
+++ QrCodeGenerator.BLL/Implementations/QrcodeGenerator.cs
@@ -0,0 +1,13 @@
+﻿using QrCodeGenerator.BLL.Interfaces;
+
+namespace QrCodeGenerator.BLL.Implementations
+{
+    public interface QrcodeGenerator : IQrcodeGenerator
+    {
+        byte[] GernerateQR(string text)
+        {
+            byte[] data = new byte[text.Length];
+            return data;
+        }
+    }
+}
