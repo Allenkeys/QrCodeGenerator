c1c48b27f238e429690fc08172d2f80725a910f2
Joshua Eze <ezejoshuac@gmail.com>
2023-03-17 23:37:20 +0100

    Added user creation custom functionality

--- QrCodeGenerator.BLL/Implementations/UserService.cs
+++ QrCodeGenerator.BLL/Implementations/UserService.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using QrCodeGenerator.BLL.Dtos;
 using QrCodeGenerator.BLL.Interfaces;
 using QrCodeGenerator.DAL.Entities;
 using QrCodeGenerator.DAL.Repository;
@@ -15,20 +18,28 @@ namespace QrCodeGenerator.BLL.Implementations
         public UserService(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
-            _userRepo = unitOfWork.GetRepository<User>();
+            _userRepo = _unitOfWork.GetRepository<User>();
+
         }
 
-        public void Create(CreateUserVM model)
+        public async Task Create(CreateUserVM model)
         {
-            _userRepo.AddAsync(new User
+            User user = await _userRepo.GetSingleByAsync(u => u.Id == model.Id);
+
+            if (user == null)
             {
-                FirstName = model.FirstName,
-                LastName = model.LastName,
-                MiddleName = model.MiddleName,
-                Email = model.Email,
-                Password = model.Password,
-                QrCodes = new List<QrCode>()
-            });
+                await _userRepo.AddAsync(new User
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    MiddleName = model.MiddleName,
+                    Email = model.Email,
+                    Password = model.Password,
+                    QrCodes = new List<QrCode>()
+                });
+                //await _unitOfWork.SaveChangesAsync();
+                await _userRepo.SaveAsync();
+            }
         }
 
         public Task<IEnumerable<User>> GetUsers()
@@ -36,9 +47,18 @@ namespace QrCodeGenerator.BLL.Implementations
             return _userRepo.GetAllAsync();
         }
 
-        public Task<IEnumerable<User>> GetUsersWithQrCodeAsync()
+        public async Task<IEnumerable<UsersWithQrVM>> GetUsersWithQrCodeAsync()
         {
-            throw new System.NotImplementedException();
+            return (await _userRepo.GetByAsync(include: u => u.Include(q => q.QrCodes))).Select(u => new UsersWithQrVM
+            {
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                QrCodes = (IEnumerable<QrCode>)u.QrCodes.Select(q => new QrCodeVM
+                {
+                    Code = q.Code,
+                    Title = q.Title
+                })
+            });
         }
     }
 }
